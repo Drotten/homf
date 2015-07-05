eab59a8304fa69469d7cd74c30c8784154257921
Mark Tibblin <mark.tibblin@gmail.com>
2015-07-05 16:42:25 +0200

    Made code look nicer.

--- ui/init.js
+++ ui/init.js
@@ -1,30 +1,30 @@
-$(top).on('stonehearthReady', function(cc) {
-
+$(top).on('stonehearthReady', function(cc)
+{
    // Initiates the config with new values if these aren't there already.
-   radiant.call('radiant:get_config', 'mods.homf').done(function(o) {
+   radiant.call('radiant:get_config', 'mods.homf').done(function(o)
+   {
       var cfg = (o || {})['mods.homf'] || {};
 
       var customizeEmbarking = cfg['customize_embarking'];
-      if (customizeEmbarking == null) {
+      if (customizeEmbarking == null)
          customizeEmbarking = true;
-      }
+
       var customizeImmigrating = cfg['customize_immigrating'];
-      if (customizeImmigrating == null) {
+      if (customizeImmigrating == null)
          customizeImmigrating = true;
-      }
+
       var zoomToHearthling = cfg['zoom_to_hearthling'];
-      if (zoomToHearthling == null) {
+      if (zoomToHearthling == null)
          zoomToHearthling = true;
-      }
+
       var pauseDuringCustomization = cfg['pause_during_customization'];
-      if (pauseDuringCustomization == null) {
+      if (pauseDuringCustomization == null)
          pauseDuringCustomization = true;
-      }
+
       /*
       var cameraFollow = cfg['camera_follow'];
-      if (cameraFollow == null) {
+      if (cameraFollow == null)
          cameraFollow = true;
-      }
       */
 
       radiant.call('radiant:set_config', 'mods.homf', {
