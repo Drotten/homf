c7e9f6414cb68462e03da04238b4ec9d136bb2df
Mark Tibblin <mark.tibblin@gmail.com>
2018-01-29 13:22:37 +0100

    Changed customize_embarking default value to false. Added enabled with default set to true.

--- ui/init.js
+++ ui/init.js
@@ -1,13 +1,11 @@
-$(top).on('stonehearthReady', function(cc)
-{
-   // Initiates the config with new values if these aren't there already.
-   radiant.call('radiant:get_config', 'mods.homf').done(function(o)
-   {
+$(top).on('stonehearthReady', function(cc) {
+   // Initiates the config with new values if these aren't there already
+   radiant.call('radiant:get_config', 'mods.homf').done(function(o) {
       var cfg = (o || {})['mods.homf'] || {};
 
       var customizeEmbarking = cfg['customize_embarking'];
       if (customizeEmbarking == null)
-         customizeEmbarking = true;
+         customizeEmbarking = false;
 
       var customizeImmigrating = cfg['customize_immigrating'];
       if (customizeImmigrating == null)
@@ -27,20 +25,24 @@ $(top).on('stonehearthReady', function(cc)
          cameraFollow = true;
       */
 
+      var enabled = cfg['enabled'];
+      if (enabled == null)
+         enabled = true;
+
       radiant.call('radiant:set_config', 'mods.homf', {
          customize_embarking: customizeEmbarking,
          customize_immigrating: customizeImmigrating,
          zoom_to_hearthling: zoomToHearthling,
-         pause_during_customization: pauseDuringCustomization
+         pause_during_customization: pauseDuringCustomization,
+         enabled: enabled,
       });
    });
 
-   // The view will be added after the town name has been decided.
+   // The view will be added after the town name has been decided
    App.gameView.views.complete.push('CustomizeHearthlingBGView');
 
    App.stonehearthClient._homfCustomizeHearthling = null;
-   App.stonehearthClient.showHomfCustomizer = function(hearthling)
-   {
+   App.stonehearthClient.showHomfCustomizer = function(hearthling) {
       // Toggle the hearthling customizer.
       if (!this._homfCustomizeHearthling || this._homfCustomizeHearthling.isDestroyed) {
          this._homfCustomizeHearthling = App.gameView.addView(App.CustomizeHearthlingView);
@@ -52,7 +54,7 @@ $(top).on('stonehearthReady', function(cc)
    };
 
 
-   // Adds a console command that begins customization on a selected hearthling.
+   // Adds a console command that begins customization on a selected hearthling
 
    var selected;
 
@@ -63,44 +65,40 @@ $(top).on('stonehearthReady', function(cc)
    radiant.console.register('homf_custom', {
       call: function(cmdobjs, fn, args) {
          var entity;
-         if (args.length > 0) {
+         if (args.length > 0)
             entity = 'object://game/' + args[0];
-         } else {
+         else
             entity = selected;
-         }
          return radiant.call('homf:force_start_customization', entity);
       },
-      description : "Customizes a hearthling. Arg 0 is id of the hearthling. If no argument is provided, customizes the currently selected hearthling. If the entity is not your own hearthling, then nothing happens. Usage: homf_custom 12345"
+      description: "Customizes a hearthling's looks. " +
+                   "Arg 0 is id of the hearthling. " +
+                   "If no argument is provided, customizes the " +
+                   "currently selected hearthling. " +
+                   "If the entity is not your own hearthling, " +
+                   "then nothing happens. Usage: homf_custom 12345"
    });
 });
 
 App.CustomizeHearthlingBGView = App.View.extend({
 
-   init: function()
-   {
+   init: function() {
       var self = this;
-      radiant.call('radiant:play_sound', {'track' : 'stonehearth:sounds:ui:start_menu:submenu_select'} );
+      radiant.call('radiant:play_sound', {'track': 'stonehearth:sounds:ui:start_menu:submenu_select'});
 
       radiant.call('homf:get_tracker')
-         .done(function(response)
-            {
-               self.trace = radiant.trace(response.tracker)
-                  .progress(function(data)
-                     {
-                        self._startCustomization(data.hearthling);
-                     }
-                  )
-                  .fail(function(e)
-                     {
-                        console.log(e);
-                     }
-                  );
-            }
-         );
+         .done(function(response) {
+            self.trace = radiant.trace(response.tracker)
+               .progress(function(data) {
+                  self._startCustomization(data.hearthling);
+               })
+               .fail(function(e) {
+                  console.log(e);
+               });
+         });
    },
 
-   _startCustomization: function(hearthling)
-   {
+   _startCustomization: function(hearthling) {
       App.stonehearthClient.showHomfCustomizer(hearthling);
    }
 });
